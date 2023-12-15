73c63b35ad2c7b929e348e98bcf2915850888565
psw1305 <shshck5@gmail.com>
2023-12-15 17:55:31 +0900

    [💡Feature] Room Generate
    - 배열에 따른 방 생성 구현
    - Room 프리팹 및 다자인

---
+++ 2DRoguelike/Assets/Scripts/Core/EnumTypes.cs
@@ -0,0 +1,18 @@
+// 레벨 생성 방향
+public enum Didirection
+{
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+// 방 타입
+public enum RoomType 
+{ 
+    Start, 
+    Normal, 
+    Boss, 
+    Treasure, 
+    Shop 
+};
