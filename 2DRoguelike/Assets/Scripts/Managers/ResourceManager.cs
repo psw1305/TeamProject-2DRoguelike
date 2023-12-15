afebb39f7f8f9ce2addd2436059e3d4a05b0f54d
psw1305 <shshck5@gmail.com>
2023-12-15 14:04:44 +0900

    [💡Feature] Managers
    - 이미지 리소스 추가 (캐릭터, 아이템, 적)
    - 리소스 폴더 경로 지정으로 파일 불러오기

--- 2DRoguelike/Assets/Scripts/Managers/ResourceManager.cs
+++ 2DRoguelike/Assets/Scripts/Managers/ResourceManager.cs
@@ -1,6 +1,37 @@
-﻿/// <summary>
-/// 리소스 매니저
-/// </summary>
+using System.Collections.Generic;
+using UnityEngine;
+
 public class ResourceManager
 {
+    private Dictionary<string, GameObject> items = new();
+    private Dictionary<string, GameObject> rooms = new();
+
+    /// <summary>
+    /// Resources 폴더 안 아이템 불러오기
+    /// </summary>
+    public void Initialize()
+    {
+        LoadPrefabs("Prefabs/Items", items);
+        LoadPrefabs("Prefabs/Rooms", rooms);
+    }
+
+    /// <summary>
+    /// 지정된 경로 안에 모든 프리팹들 로드
+    /// </summary>
+    /// <param name="path">폴더 경로</param>
+    /// <param name="prefabs">로드할 프리팹 필드값</param>
+    public void LoadPrefabs(string path, Dictionary<string, GameObject> prefabs)
+    {
+        GameObject[] objs = Resources.LoadAll<GameObject>(path);
+        foreach (GameObject obj in objs)
+        {
+            prefabs[obj.name] = obj;
+        }
+    }
+
+    public GameObject Instantiate(Dictionary<string, GameObject> list, string prefabName)
+    {
+        if (!list.TryGetValue(prefabName, out GameObject prefab)) return null;
+        return GameObject.Instantiate(prefab);
+    }
 }
