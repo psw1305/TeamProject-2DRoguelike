fec0e0dc43e75e45eb93c52abe46dbc8fddaaa19
psw1305 <shshck5@gmail.com>
2023-12-18 11:13:38 +0900

    [💡Feature] Player
    - Player 클래스 정의 및 생성

--- 2DRoguelike/Assets/Scripts/Level/Room.cs
+++ 2DRoguelike/Assets/Scripts/Level/Room.cs
@@ -1,6 +1,42 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public Vector2 coordinate;
+    #region Fields
+
+    [SerializeField] private Vector2Int coordinate;
+    [SerializeField] private List<GameObject> doorList;
+
+    private List<GameObject> activeDoorList = new();
+    private List<GameObject> neighborDoorList = new();
+    private List<Room> neighborRoomList = new();
+
+    #endregion
+
+    #region Properties
+
+    public Vector2Int Coordinate { get; set; }
+    public List<GameObject> DoorList => doorList;
+
+    #endregion
+
+    #region Methods
+
+    public void ActivationDoor(RoomDirection direction, Room neighborRoom, GameObject neighborDoor)
+    {
+        var door = doorList[(int)direction];
+
+        for (int i = 0; i < door.transform.childCount; i++)
+        {
+            door.transform.GetChild(i).gameObject.SetActive(true);
+        }
+
+        activeDoorList.Add(door);
+        neighborDoorList.Add(neighborDoor);
+        neighborRoomList.Add(neighborRoom);    
+    }
+
+    #endregion
+
 }
