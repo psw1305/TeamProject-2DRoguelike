5d534bbe3fa796e0b04cf19d2c74263a7ebc9fca
Ku-dodo <rnehgus9403@gmail.com>
2023-12-18 11:26:10 +0900

    [🛠️Fix] Obstacle Active ,Destroy Logic Change
    + 장애물의 활성화, 파괴가 들어온 Collsion의 Layer에 따라 처리되도록 변경

--- 2DRoguelike/Assets/Scripts/Obstacle/Obstacle.cs
+++ 2DRoguelike/Assets/Scripts/Obstacle/Obstacle.cs
@@ -1,17 +1,25 @@
+using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour, IGenerateReward
 {
     [SerializeField][Range(0f, 100f)] 
     private int RewardDropRate;
-    
-    [SerializeField] private GameObject rewardTable;
-    private GameObject mainObj;
+
+    [SerializeField] private ParticleSystem destroyParticle;
+
+    [SerializeField] private LayerMask canDestroyLayer;
+
+    private GameObject _mainObj;
 
     protected virtual void DestroySelf()
     {
-        mainObj = transform.parent.gameObject;
-        Destroy(mainObj);
+        Instantiate(destroyParticle, gameObject.transform.position, Quaternion.identity);
+        destroyParticle.Play();
+
+        _mainObj = transform.parent.gameObject;
+        Destroy(_mainObj);
     }
 
     public void GenerateReward()
@@ -22,5 +30,12 @@ public class Obstacle : MonoBehaviour, IGenerateReward
             Debug.Log("보상 생성됨" + transform.position.x + transform.position.y);
         }
         Debug.Log(ran);
+    }
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (canDestroyLayer.value == (canDestroyLayer.value | (1 << collision.gameObject.layer)))
+        {
+            DestroySelf();
+        }
     }
 }
--- 2DRoguelike/Assets/Scripts/Obstacle/SpikeTrap.cs
+++ 2DRoguelike/Assets/Scripts/Obstacle/SpikeTrap.cs
@@ -4,31 +4,36 @@ using UnityEngine;
 
 public class SpikeTrap : Obstacle
 {
-    [SerializeField] private int trapAttack;
+    [SerializeField] private LayerMask activeTargetLayer;
+
+    [SerializeField] private int trapDamage;
     [SerializeField] private float trapActiveDelay;
     private Animator _animator;
+
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    
+    protected override void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (activeTargetLayer.value == (activeTargetLayer | (1 << collision.gameObject.layer)))
         {
             Invoke("ActiveTrap", trapActiveDelay);
         }
+        base.OnTriggerEnter2D(collision);
     }
-    
+
     private void ActiveTrap()
     {
         _animator.SetBool("isActive", true);
     }
 
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         _animator.SetBool("isActive", false);
+        CancelInvoke();
     }
+
 }
