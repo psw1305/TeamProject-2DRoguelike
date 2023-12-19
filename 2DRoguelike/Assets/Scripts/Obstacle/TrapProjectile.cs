621dfadbbe8975860b70a4e31b78ee6248f2cb37
Ku-dodo <rnehgus9403@gmail.com>
2023-12-19 17:34:43 +0900

    [💡Feature] Add SpawnTrap
    + 파괴되면 근접 몬스터가 소환되는 트랩 추가

--- 2DRoguelike/Assets/Scripts/Obstacle/TrapProjectile.cs
+++ 2DRoguelike/Assets/Scripts/Obstacle/TrapProjectile.cs
@@ -7,20 +7,20 @@ public class RangeTrapBullet : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("P!");
-            Main.Object.Despawn(this);
-        }
-        else if (collision.gameObject.CompareTag("Wall"))
-        {
-            Debug.Log("W!");
-            Main.Object.Despawn(this);
-        }
-        else if(collision.gameObject.CompareTag("DoorCollider"))
-        {
-            Debug.Log("D!");
-            Main.Object.Despawn(this);
-        }
+        //if (collision.gameObject.CompareTag("Player"))
+        //{
+        //    Debug.Log("P!");
+        //    Main.Object.Despawn(this);
+        //}
+        //if (collision.gameObject.CompareTag("Wall"))
+        //{
+        //    Debug.Log("W!");
+        //    Main.Object.Despawn(this);
+        //}
+        //if(collision.gameObject.CompareTag("DoorCollider"))
+        //{
+        //    Debug.Log("D!");
+        //    Main.Object.Despawn(this);
+        //}
     }
 }
