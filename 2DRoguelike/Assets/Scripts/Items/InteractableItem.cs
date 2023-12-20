2923e3d6827c31a06fa5618d196a014153042900
YongTaeJ <g_o_robot@naver.com>
2023-12-20 14:26:24 +0900

    [➕Add] Items, rooms
    1. 연습용 방 추가
    2. 아이템 획득 소리 추가(연결은 X)
    3. 아이템 가짓수 추가

--- 2DRoguelike/Assets/Scripts/Items/InteractableItem.cs
+++ 2DRoguelike/Assets/Scripts/Items/InteractableItem.cs
@@ -1,38 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class InteractableItem : BaseItem
 {
+    protected EquipSystem _equipSystem = new EquipSystem();
+    protected InteractableItemBluePrint targetBp;
+
     public override void SetItem(ItemBlueprint blueprint)
     {
         base.SetItem(blueprint);
-        InteractableItemBluePrint target = blueprint as InteractableItemBluePrint;
-        GetComponentInChildren<SpriteRenderer>().sprite = target.itemSprite;
+        targetBp = blueprint as InteractableItemBluePrint;
+        GetComponentInChildren<SpriteRenderer>().sprite = targetBp.itemSprite;
     }
 
-    public virtual bool PlayerGetThis()
-    {
-        return true;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    // TODO => 충돌시 바로 구매 후 스탯 반영이 되도록
+    protected virtual void OnTriggerEnter2D(Collider2D other)
     {
         if(!other.CompareTag("Player")) return;
-
-        other.GetComponent<ItemInteract>().CurrentItem = this;
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if(!other.CompareTag("Player")) return;
-
-        ItemInteract itemInteract = other.GetComponent<ItemInteract>();
-        if(itemInteract.CurrentItem == this) itemInteract.CurrentItem = null;
-    }
-
-    public InteractableItemBluePrint GetBlueprint()
-    {
-        return blueprint as InteractableItemBluePrint;
+        _equipSystem.GetItemStatus(targetBp);
+        // TODO => 소리 출력
+        Destroy(gameObject);
     }
 }
