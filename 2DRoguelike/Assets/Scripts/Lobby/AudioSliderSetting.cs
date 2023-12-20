89315470ca7df5987a65db053affcd86eb2fed7e
Ku-dodo <rnehgus9403@gmail.com>
2023-12-20 14:05:27 +0900

    [➕Add] Sound Setting
    + 소리 볼륨 저장 및 불러오기
    + 사운드 클립 추가

--- 2DRoguelike/Assets/Scripts/Lobby/AudioSliderSetting.cs
+++ 2DRoguelike/Assets/Scripts/Lobby/AudioSliderSetting.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AudioSliderSetting : MonoBehaviour
 {
-    [SerializeField] Slider audioSettingSlider;
+    [SerializeField] private Slider audioSettingSlider;
+
+    [SerializeField] private string PlayerPrefsKey;
+    private float _playerPrefsValue;
+
+    private void Awake()
+    {
+        InitSlider();
+    }
+    private void InitSlider()
+    {
+        _playerPrefsValue = PlayerPrefs.GetFloat(PlayerPrefsKey);
+        audioSettingSlider.value = _playerPrefsValue;
+    }
 
     public void BGMAudioControl(float sound)
     {
         sound = audioSettingSlider.value;
-        //AudioSystem<BGM>.Instance.VolumeScale = sound;
-        PlayerPrefs.SetFloat("SFXSoundValue", sound);
+        AudioSystem<BGM>.Instance.VolumeScale = sound;
+        PlayerPrefs.SetFloat("BGMSoundValue", sound);
     }
 
     public void SFXAudioControl(float sound)
     {
         sound = audioSettingSlider.value;
-        //AudioSystem<SFX>.Instance.VolumeScale = sound;
-        PlayerPrefs.SetFloat("BGMSoundValue", sound);
+        AudioSystem<SFX>.Instance.VolumeScale = sound;
+        PlayerPrefs.SetFloat("SFXSoundValue", sound);
     }
 }
